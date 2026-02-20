a03edab19825bd8c244024f181d91099a2d5fa29
Marek Skacelik <marek.skacelik@gmail.com>
2026-02-20 17:39:42 +0100

    Fix prefixes on routes, Add stork and TLS routes for edgy

--- edgy-gateway/src/main/java/com/edgy/EdgyGateway.java
+++ edgy-gateway/src/main/java/com/edgy/EdgyGateway.java
@@ -11,8 +11,15 @@ class EdgyGateway {
     @Produces
     RoutingConfiguration routing() {
         return new RoutingConfiguration()
-                .addRoute(new Route("/api/villains",
-                        Origin.of("villain-backend", "http://villains-service-1:8080/villains"),
+                .addRoute(new Route("/api/villains/*",
+                                Origin.of("villain-backend",
+                                "http://villains-service-1:8080/villains/{__REQUEST_URI_AFTER_PREFIX__}"),
+                        PathMode.PREFIX))
+                .addRoute(new Route("/api/stork/villains/*",
+                        Origin.of("villain-backends", "stork://villains-service/villains/{__REQUEST_URI_AFTER_PREFIX__}"),
+                        PathMode.PREFIX))
+                .addRoute(new Route("/api/heroes/*",
+                        Origin.of("hero-backend", "https://heroes-service:8443/heroes/{__REQUEST_URI_AFTER_PREFIX__}"),
                         PathMode.PREFIX));
     }
 }
